ce157d032a1d615784bdf7b866fe986d4de01b1b
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-27 19:29:48 +0000

    submission3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
 let[showpara,setShowpara]=useState(false);
+useEffect(()=>{
+  <p className="hide show"></p>
+});
   return (
     <div className="App" id="main">
       // Do not alter the main div
