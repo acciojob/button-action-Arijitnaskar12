40f87aa83c6bbbc7cced1f5d02915edd12c6ec70
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-27 17:43:36 +0000

    Submission

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+let[showpara,setShowpara]=useState(false);
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <button onClick={()=>setShowpara(true)}>Clickme</button>
+      {
+        showpara && (
+          <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        )
+      }
     </div>
   );
 }
